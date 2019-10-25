9a737069f389d72597e164c0501ba53c1cc43104
Kairat <kairstos@gmail.com>
2019-10-25 10:22:43 +0600

    Part2task3

---
+++ t3.py
@@ -0,0 +1,5 @@
+students = int(input('Введите количество студентов:'))
+apples = int(input('Введите количество яблок:'))
+print(f"Каждый студент получит по {apples//students},\n {apples%students}яблок остаток")
+
+
